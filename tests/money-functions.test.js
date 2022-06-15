18f96f059db4d626646f68a278a40facbc30d3ae
Marlee Garcia <marlee.garcia121@gmail.com>
2022-06-15 17:11:56 -0400

    added notes to test functions

--- tests/money-functions.test.js
+++ tests/money-functions.test.js
@@ -2,100 +2,136 @@ const {formatCurrency, getCoins} = require("../src/js/money-functions");
 
 describe("formatCurrency", () => {
   test("Given the amount 0, it returns $0.00.", () => {
+    // Arrange
     const amount = 0;
 
+    // Act
     const formattedCurrency = formatCurrency(amount);
 
+    // Assert
     expect(formattedCurrency).toBe("$0.00");
   });
 
   test("Given the amount 1, it returns $1.00", () => {
+    // Arrange
     const amount = 1;
 
+    // Act
     const formattedCurrency = formatCurrency(amount);
 
+    // Assert
     expect(formattedCurrency).toBe("$1.00");
   });
 
   test("Given the amount 1.5, it returns $1.50", () => {
+    // Arrange
     const amount = 1.5;
 
+    // Act
     const formattedCurrency = formatCurrency(amount);
 
+    // Assert
     expect(formattedCurrency).toBe("$1.50");
   });
 
   test("Given the amount 0.01, it returns $0.01", () => {
+    // Arrange
     const amount = 0.01;
 
+    // Act
     const formattedCurrency = formatCurrency(amount);
 
+    // Assert
     expect(formattedCurrency).toBe("$0.01");
   });
 
   test("Given the amount 527.6789, it returns $$527.68", () => {
+    // Arrange
     const amount = 527.6789;
 
+    // Act
     const formattedCurrency = formatCurrency(amount);
 
+    // Assert
     expect(formattedCurrency).toBe("$527.68");
   });
 
   test("Given the amount -1, it returns -$1.00", () => {
+    // Arrange
     const amount = -1;
 
+    // Act
     const formattedCurrency = formatCurrency(amount);
 
+    // Assert
     expect(formattedCurrency).toBe("-$1.00");
   });
 
   test("Given the amount 100, it returns $100.00", () => {
+    // Arrange
     const amount = 100;
 
+    // Act
     const formattedCurrency = formatCurrency(amount);
 
+    // Assert
     expect(formattedCurrency).toBe("$100.00");
   });
 
   test("Given the amount -50.5, it returns -$50.50", () => {
+    // Arrange
     const amount = -50.5;
 
+    // Act
     const formattedCurrency = formatCurrency(amount);
 
+    // Assert
     expect(formattedCurrency).toBe("-$50.50");
   });
 });
 
 describe("getCoins", () => {
   test("32 cents produces: quarters: 1, dimes: 0, nickels: 1, pennies: 2", () => {
+    // Arrange 
     const cents = 32;
 
+    // Act
     const coins = getCoins(cents);
 
+    // Assert
     expect(coins).toStrictEqual({quarters: 1, dimes: 0, nickels: 1, pennies: 2});
   });
 
   test("10 cents produces: quarters: 0, dimes: 1, nickels: 0, pennies: 0.", () => {
+    // Arrange
     const cents = 10;
 
+    // Act
     const coins = getCoins(cents);
 
+    // Assert
     expect(coins).toStrictEqual({quarters: 0, dimes: 1, nickels: 0, pennies: 0});
   });
 
   test("27 cents produces: quarters: 1, dimes: 0, nickels: 0, pennies: 2.", () => {
+    // Arrange
     const cents = 27;
 
+    // Act
     const coins = getCoins(cents);
 
+    // Assert
     expect(coins).toStrictEqual({quarters: 1, dimes: 0, nickels: 0, pennies: 2});
   });
 
   test("68 cents produces: quarters: 2, dimes: 1, nickels: 1, pennies: 3.", () => {
+    // Arrange
     const cents = 68;
 
+    // Act
     const coins = getCoins(cents);
 
+    // Assert
     expect(coins).toStrictEqual({quarters: 2, dimes: 1, nickels: 1, pennies: 3});
   });
 });
